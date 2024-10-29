3d6cc311be9f7d26822eef975fd03ed25c31792c
Biechiwonz <zhangzhenfu1234@163.com>
2024-10-29 16:52:58 +0800

    19
    Signed-off-by: Biechiwonz <zhangzhenfu1234@163.com>

--- src/LookingGlassControls.ts
+++ src/LookingGlassControls.ts
@@ -82,7 +82,7 @@ export function initLookingGlassControlGUI() {
 		help.style.color = "rgba(255,255,255,0.7)"
 		help.style.fontSize = "14px"
 		help.style.margin = "5px 0"
-		help.innerHTML = "mousetest18 Click the popup and use WASD, mouse left/right drag, and scroll."
+		help.innerHTML = "mousetest19 Click the popup and use WASD, mouse left/right drag, and scroll."
 	
 		const controlListDiv = document.createElement("div")
 		c.appendChild(controlListDiv)
@@ -255,7 +255,7 @@ export function initLookingGlassControlGUI() {
 				cfg.targetY = cfg.targetY + dy * cfg.targetDiam * 0.001
 				cfg.targetZ = cfg.targetZ + dz * cfg.targetDiam * 0.001
 			} else {
-				if (cfg.trackballY < 10) {
+				if (cfg.trackballY > 5) {
 					cfg.trackballY = cfg.trackballY - my * 0.01
 					cfg.trackballX = cfg.trackballX - mx * 0.01
 				}
@@ -323,7 +323,7 @@ export function initLookingGlassControlGUI() {
 				cfg.targetY = cfg.targetY + dy * cfg.targetDiam * 0.001
 				cfg.targetZ = cfg.targetZ + dz * cfg.targetDiam * 0.001
 			} else {
-				if (cfg.trackballY < 10) {
+				if (cfg.trackballY > 5) {
 					cfg.trackballY = cfg.trackballY - my * 0.01
 					cfg.trackballX = cfg.trackballX - mx * 0.01 }
 
