a3ff652d2443fb963994636e69869ca40fb2d774
Biechiwonz <zhangzhenfu1234@163.com>
2024-10-29 17:04:48 +0800

    Signed-off-by: Biechiwonz <zhangzhenfu1234@163.com>

--- src/LookingGlassControls.ts
+++ src/LookingGlassControls.ts
@@ -255,10 +255,10 @@ export function initLookingGlassControlGUI() {
 				cfg.targetY = cfg.targetY + dy * cfg.targetDiam * 0.001
 				cfg.targetZ = cfg.targetZ + dz * cfg.targetDiam * 0.001
 			} else {
-				if (cfg.trackballX <0.8) {
-					cfg.trackballY = cfg.trackballY - my * 0.01
-					cfg.trackballX = cfg.trackballX - mx * 0.01
-				}
+				
+				cfg.trackballY = cfg.trackballY - my * 0.01
+				cfg.trackballX = cfg.trackballX - mx * 0.01
+			
 
 
 
